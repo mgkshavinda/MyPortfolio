26a54870425f3ea220444e2292e35864c814001e
Kavindu Shavinda <96062995+MGKShavinda@users.noreply.github.com>
2023-11-14 22:23:43 +0530

    Added Hero Animation
    Added Hero Animation to main.js

---
+++ components/js/main.js
@@ -0,0 +1,9 @@
+// Hero Section Effect
+document.addEventListener('DOMContentLoaded', function() {
+    var typed = new Typed('.typed', {
+        strings: ["Undergraduate", "Aspiring Software Engineer", "Designer", "Developer"],
+        typeSpeed: 120, // typing speed in milliseconds
+        backSpeed: 40, // backspacing speed in milliseconds
+        loop: true // loop the animation
+    });
+});
