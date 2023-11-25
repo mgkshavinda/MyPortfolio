4dd0dabf614a2479ad1890f6b566861991c399e9
Kavindu Shavinda <mgkshavinda@gmail.com>
2023-11-25 22:37:38 +0530

    Added Formspree Account
    Added Formspree Account, Created a Toast, Updated Download button with G-Drive link

--- components/js/main.js
+++ components/js/main.js
@@ -7,3 +7,53 @@ document.addEventListener('DOMContentLoaded', function() {
         loop: true // loop the animation
     });
 });
+
+
+// Download Button 
+document.getElementById("resume-btn").addEventListener("click", function() {
+    // shareable link of CV file
+    var downloadLink = 'https://drive.google.com/file/d/1PRfJwKt6VeU1b8u3qZaefty-J9BDy9RO/view?usp=sharing';
+    window.open(downloadLink, '_blank');
+});
+
+
+// Formspree Form  https://formspree.io/f/xrgwwegb
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevent the default form submission behavior
+
+    var form = e.target;
+    var formData = new FormData(form);
+
+    // Formspree endpoint
+    var formAction = 'https://formspree.io/f/xrgwwegb';
+
+    fetch(formAction, {
+        method: form.method,
+        body: formData,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            // Handle success with Bootstrap Toast
+            showToast('Error sending message. Please try again.', 'bg-danger');
+        } else {
+            // Handle error with Bootstrap Toast
+        
+            showToast('Your message has been sent. Thank you!', 'bg-success');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        // Handle error with Bootstrap Toast
+        showToast('Error sending message. Please try again.', 'bg-danger');
+    });
+});
+
+function showToast() {
+    var toastEl = document.getElementById('liveToast');
+    var toast = new bootstrap.Toast(toastEl);
+    toast.show();
+  }
