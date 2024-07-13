eb26e9ef5ad8b57d745cc9583a8c0abb945c6c3a
Kavindu Shavinda <kavindu2015@gmail.com>
2024-07-14 01:28:44 +0530

    Updated Skills Section
    Updated Skills Section

--- components/js/main.js
+++ components/js/main.js
@@ -45,7 +45,7 @@ document.addEventListener("DOMContentLoaded", function () {
 // Hero Section Effect
 document.addEventListener('DOMContentLoaded', function() {
     var typed = new Typed('.typed', {
-        strings: ["Undergraduate", "Aspiring Software Engineer", "Designer", "Developer"],
+        strings: ["Software Engineer Intern", "Undergraduate", "Designer", "Developer"],
         typeSpeed: 120, // typing speed in milliseconds
         backSpeed: 40, // backspacing speed in milliseconds
         loop: true // loop the animation
